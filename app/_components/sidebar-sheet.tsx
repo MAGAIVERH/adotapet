d24ef761c5339ca7b883c0514d54486215eb17ca
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-10-20 10:23:13 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -5,12 +5,18 @@ import {
   SheetTitle,
 } from '@/components/ui/sheet'
 
-import { HomeIcon, CalendarIcon, LogOutIcon } from 'lucide-react'
+import { HomeIcon, CalendarIcon, LogOutIcon, LogInIcon } from 'lucide-react'
 import { Button } from './ui/button'
 import { quickSearchOptions } from '../_constants/search'
 import Image from 'next/image'
-import { Avatar, AvatarImage } from './ui/avatar'
+
 import Link from 'next/link'
+import { Dialog, DialogContent, DialogHeader } from '@/components/ui/dialog'
+import {
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from '@radix-ui/react-dialog'
 
 const SidebarSheet = () => {
   return (
@@ -19,15 +25,34 @@ const SidebarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
-          <AvatarImage src="https://plus.unsplash.com/premium_photo-1672239496290-5061cfee7ebb?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-        </Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="text-lg font-bold">Olá faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
 
-        <div>
-          <p className="font-bold">Magaiver Magalhaes</p>
-          <p className="text-xs">magaivermagalhaes@hotmail.com</p>
-        </div>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image
+                src="/icone-google.png"
+                width={18}
+                height={18}
+                alt="Fazer login com o Google"
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
