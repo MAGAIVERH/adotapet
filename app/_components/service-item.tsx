6375e8a418572c0ab55c6828f820d771dbf2c8a6
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-10-30 23:29:40 -0300

    feat: ensure only available times are shown on booking process

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -1,25 +1,24 @@
 'use client'
 
-import { Personal, PersonalService } from '@prisma/client'
+import { Booking, Personal, PersonalService } from '@prisma/client'
 import Image from 'next/image'
 import { Button } from './ui/button'
 import { Card, CardContent } from './ui/card'
 import {
   Sheet,
-  SheetClose,
   SheetContent,
   SheetFooter,
   SheetHeader,
   SheetTitle,
-  SheetTrigger,
 } from '@/components/ui/sheet'
 import { Calendar } from './ui/calendar'
 import { ptBR } from 'date-fns/locale'
-import { useState } from 'react'
-import { format, set } from 'date-fns'
+import { useEffect, useState } from 'react'
+import { addDays, format, set } from 'date-fns'
 import { createBooking } from '../_actions/create-booking'
 import { toast } from 'sonner'
 import { useSession } from 'next-auth/react'
+import { getBookings } from '../_actions/get-bookings'
 
 interface ServiceItemProps {
   service: PersonalService
@@ -43,12 +42,51 @@ const TIME_LIST = [
   '21:00',
 ]
 
+const getTimeList = (bookings: Booking[]) => {
+  return TIME_LIST.filter((time) => {
+    const hour = Number(time.split(':')[0])
+    const minutes = Number(time.split(':')[1])
+
+    const hasBookingOnCurrentTime = bookings.some(
+      (booking) =>
+        booking.date.getHours() === hour &&
+        booking.date.getMinutes() === minutes
+    )
+    if (hasBookingOnCurrentTime) {
+      return false
+    }
+    return true
+  })
+}
+
 const ServiceItem = ({ service, personal }: ServiceItemProps) => {
   const { data } = useSession()
   const [selectedDay, setSelectedDay] = useState<Date | undefined>(undefined)
   const [selectedTime, setSelectedTime] = useState<string | undefined>(
     undefined
   )
+
+  const [dayBookings, setDayBookings] = useState<Booking[]>([])
+  const [bookingSheetIsOpen, setBookingSheetIsOpen] = useState(false)
+
+  useEffect(() => {
+    const fetch = async () => {
+      if (!selectedDay) return
+      const bookings = await getBookings({
+        date: selectedDay,
+        serviceId: service.id,
+      })
+      setDayBookings(bookings)
+    }
+    fetch()
+  }, [selectedDay, service.id])
+
+  const handleBookingSheetOpenChange = () => {
+    setSelectedDay(undefined)
+    setSelectedTime(undefined)
+    setDayBookings([])
+    setBookingSheetIsOpen(false)
+  }
 
   const handleDateSelect = (date: Date | undefined) => {
     setSelectedDay(date)
@@ -76,6 +114,7 @@ const ServiceItem = ({ service, personal }: ServiceItemProps) => {
         userId: (data?.user as any).id,
         date: newDate,
       })
+      handleBookingSheetOpenChange()
       toast.success('Reserva criada com sucesso!')
     } catch (error) {
       console.log(error)
@@ -110,16 +149,18 @@ const ServiceItem = ({ service, personal }: ServiceItemProps) => {
               }).format(Number(service.price))}
             </p>
 
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button
-                  variant="secondary"
-                  className="hover:bg-[hsl(var(--primary))]"
-                  size="sm"
-                >
-                  Reserver
-                </Button>
-              </SheetTrigger>
+            <Sheet
+              open={bookingSheetIsOpen}
+              onOpenChange={handleBookingSheetOpenChange}
+            >
+              <Button
+                variant="secondary"
+                className="hover:bg-[hsl(var(--primary))]"
+                size="sm"
+                onClick={() => setBookingSheetIsOpen(true)}
+              >
+                Reserver
+              </Button>
 
               <SheetContent className="px-0">
                 <SheetHeader>
@@ -132,6 +173,7 @@ const ServiceItem = ({ service, personal }: ServiceItemProps) => {
                     locale={ptBR}
                     selected={selectedDay}
                     onSelect={handleDateSelect}
+                    fromDate={addDays(new Date(), 1)}
                     styles={{
                       head_cell: {
                         width: '100%',
@@ -160,7 +202,7 @@ const ServiceItem = ({ service, personal }: ServiceItemProps) => {
 
                 {selectedDay && (
                   <div className="flex gap-3 overflow-auto border-b border-solid p-5 [&::-webkit-scrollbar]:hidden">
-                    {TIME_LIST.map((time) => (
+                    {getTimeList(dayBookings).map((time) => (
                       <Button
                         key={time}
                         variant={selectedTime === time ? 'default' : 'outline'}
@@ -211,14 +253,12 @@ const ServiceItem = ({ service, personal }: ServiceItemProps) => {
                 )}
 
                 <SheetFooter className="mt-5 px-5">
-                  <SheetClose asChild>
-                    <Button
-                      onClick={handleCreateBooking}
-                      disabled={!selectedDay || !selectedTime}
-                    >
-                      Confirmar
-                    </Button>
-                  </SheetClose>
+                  <Button
+                    onClick={handleCreateBooking}
+                    disabled={!selectedDay || !selectedTime}
+                  >
+                    Confirmar
+                  </Button>
                 </SheetFooter>
               </SheetContent>
             </Sheet>
