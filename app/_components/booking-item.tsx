4ff5e8e5c8178e628d9d4d988a5e41f1a533cfce
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-11-02 15:46:45 -0300

    feat: ensure booking details can be viewed

--- app/_components/booking-item.tsx
+++ app/_components/booking-item.tsx
@@ -4,6 +4,15 @@ import { Badge } from './ui/badge'
 import { Card, CardContent } from './ui/card'
 import { format, isFuture } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+import Image from 'next/image'
+import PhoneItem from './phone-item'
 
 interface BookingItemProps {
   booking: Prisma.BookingGetPayload<{
@@ -21,43 +30,123 @@ const BookingItem = ({ booking }: BookingItemProps) => {
   const isConfirmed = isFuture(booking.date)
 
   return (
-    <>
-      <Card className="min-w-[90%]">
-        <CardContent className="flex justify-between p-0">
-          {/*Esquerda*/}
-          <div className="flex flex-col gap-2 py-5 pl-5">
-            <Badge
-              className="w-fit"
-              variant={isConfirmed ? 'default' : 'secondary'}
-            >
-              {isConfirmed ? 'Confirmado' : 'Finalizado'}
-            </Badge>
-            <h3>{booking.service.name}</h3>
+    <Sheet>
+      <SheetTrigger className="w-full">
+        <Card className="min-w-[90%]">
+          <CardContent className="flex justify-between p-0">
+            {/*Esquerda*/}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge
+                className="w-fit"
+                variant={isConfirmed ? 'default' : 'secondary'}
+              >
+                {isConfirmed ? 'Confirmado' : 'Finalizado'}
+              </Badge>
+              <h3>{booking.service.name}</h3>
 
-            <div className="flex items-center gap-2">
-              <Avatar className="h-6 w-6">
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src={booking.service.personal.imageUrl} />
+                </Avatar>
+
+                <p className="text-sm">{booking.service.personal.name}</p>
+              </div>
+            </div>
+
+            {/*Direita*/}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm capitalize">
+                {format(booking.date, 'MMMM', { locale: ptBR })}
+              </p>
+              <p className="text-2xl">
+                {format(booking.date, 'dd', { locale: ptBR })}
+              </p>
+              <p className="text-sm">
+                {format(booking.date, 'HH:mm', { locale: ptBR })}
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+      </SheetTrigger>
+
+      <SheetContent className="w-[90%]">
+        <SheetHeader>
+          <SheetTitle className="text-left">Informações da Reserva</SheetTitle>
+        </SheetHeader>
+
+        <div className="relative mt-6 flex h-[180px] w-full items-end">
+          <Image
+            src="/map.png"
+            fill
+            className="rounded-xl object-cover"
+            alt={`Mapa da Academia onde trabalha o personal ${booking.service.personal.name}`}
+          />
+
+          <Card className="z-50 mx-5 mb-3 w-full rounded-xl">
+            <CardContent className="flex items-center gap-3 px-5 py-3">
+              <Avatar>
                 <AvatarImage src={booking.service.personal.imageUrl} />
               </Avatar>
 
-              <p className="text-sm">{booking.service.personal.name}</p>
-            </div>
-          </div>
+              <div className="">
+                <h3 className="font-bold">{booking.service.personal.name}</h3>
+                <p className="text-xs">{booking.service.personal.address}</p>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
 
-          {/*Direita*/}
-          <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-            <p className="text-sm capitalize">
-              {format(booking.date, 'MMMM', { locale: ptBR })}
-            </p>
-            <p className="text-2xl">
-              {format(booking.date, 'dd', { locale: ptBR })}
-            </p>
-            <p className="text-sm">
-              {format(booking.date, 'HH:mm', { locale: ptBR })}
-            </p>
+        <div className="mt-6">
+          <Badge
+            className="w-fit"
+            variant={isConfirmed ? 'default' : 'secondary'}
+          >
+            {isConfirmed ? 'Confirmado' : 'Finalizado'}
+          </Badge>
+
+          <Card className="mb-6 mt-3">
+            <CardContent className="space-y-3 p-3">
+              <div className="flex items-center justify-between">
+                <h2 className="font-bold">{booking.service.name}</h2>
+                <p className="text-sm font-bold">
+                  {Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  }).format(Number(booking.service.price))}
+                </p>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <h2 className="text-sm text-gray-400">Data</h2>
+                <p className="text-sm">
+                  {format(booking.date, "d 'de' MMMM", {
+                    locale: ptBR,
+                  })}
+                </p>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <h2 className="text-sm text-gray-400">Horário</h2>
+                <p className="text-sm">
+                  {format(booking.date, 'HH:mm', { locale: ptBR })}
+                </p>
+              </div>
+
+              <div className="flex items-center justify-between">
+                <h2 className="text-sm text-gray-400">Personal</h2>
+                <p className="text-sm">{booking.service.personal.name}</p>
+              </div>
+            </CardContent>
+          </Card>
+
+          <div className="space-y-3">
+            {booking.service.personal.phones.map((phone) => (
+              <PhoneItem key={phone} phone={phone} />
+            ))}
           </div>
-        </CardContent>
-      </Card>
-    </>
+        </div>
+      </SheetContent>
+    </Sheet>
   )
 }
 
