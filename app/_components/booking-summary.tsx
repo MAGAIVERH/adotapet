df45a58359f5676588c4f4b5f1bd40980f29e8a0
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-11-03 19:48:45 -0300

    feat: add BookingSummary component

--- app/_components/booking-summary.tsx
+++ app/_components/booking-summary.tsx
@@ -1,7 +1,53 @@
-// const BookingSummary = () => {
-//     return (
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { Card, CardContent } from './ui/card'
+import { Personal, PersonalService } from '@prisma/client'
 
-//      );
-// }
+interface BookingSummaryProps {
+  service: Pick<PersonalService, 'name' | 'price'>
+  personal: Pick<Personal, 'name'>
+  selectedDate: Date
+}
 
-// export default BookingSummary;
+const BookingSummary = ({
+  service,
+  personal,
+  selectedDate,
+}: BookingSummaryProps) => {
+  return (
+    <Card>
+      <CardContent className="space-y-3 p-3">
+        <div className="flex items-center justify-between">
+          <h2 className="font-bold">{service.name}</h2>
+          <p className="text-sm font-bold">
+            {Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL',
+            }).format(Number(service.price))}
+          </p>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <h2 className="text-sm text-gray-400">Data</h2>
+          <p className="text-sm">
+            {format(selectedDate, "d 'de' MMMM", {
+              locale: ptBR,
+            })}
+          </p>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <h2 className="text-sm text-gray-400">Horário</h2>
+          <p className="text-sm">{format(selectedDate, 'HH:mm')}</p>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <h2 className="text-sm text-gray-400">Personal</h2>
+          <p className="text-sm">{personal.name}</p>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default BookingSummary
--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -14,13 +14,14 @@ import {
 import { Calendar } from './ui/calendar'
 import { ptBR } from 'date-fns/locale'
 import { useEffect, useMemo, useState } from 'react'
-import { format, set, isPast, isToday } from 'date-fns'
+import { set, isPast, isToday } from 'date-fns'
 import { createBooking } from '../_actions/create-booking'
 import { toast } from 'sonner'
 import { useSession } from 'next-auth/react'
 import { getBookings } from '../_actions/get-bookings'
 import { Dialog, DialogContent } from '@/components/ui/dialog'
 import SignInDialog from './sign-in-dialog'
+import BookingSummary from './booking-summary'
 
 interface ServiceItemProps {
   service: PersonalService
@@ -94,6 +95,14 @@ const ServiceItem = ({ service, personal }: ServiceItemProps) => {
     fetch()
   }, [selectedDay, service.id])
 
+  const selectedDate = useMemo(() => {
+    if (!selectedDay || !selectedTime) return
+    return set(selectedDay, {
+      hours: Number(selectedTime?.split(':')[0]),
+      minutes: Number(selectedTime?.split(':')[1]),
+    })
+  }, [selectedDay, selectedTime])
+
   const handleBookingClick = () => {
     if (data?.user) {
       return setBookingSheetIsOpen(true)
@@ -117,20 +126,12 @@ const ServiceItem = ({ service, personal }: ServiceItemProps) => {
   }
 
   const handleCreateBooking = async () => {
-    // 1.Nao exibir horarios que ja foram agendados
-    // 2.Nao deixar o usuario reservar se nao estiver logado
     try {
-      if (!selectedDay || !selectedTime) return
-      // ["09" : "00"]
-      const hour = Number(selectedTime.split(':')[0])
-      const minute = Number(selectedTime.split(':')[1])
-      const newDate = set(selectedDay, {
-        minutes: Number(minute),
-        hours: Number(hour),
-      })
+      if (!selectedDate) return
+
       await createBooking({
         serviceId: service.id,
-        date: newDate,
+        date: selectedDate,
       })
       handleBookingSheetOpenChange()
       toast.success('Reserva criada com sucesso!')
@@ -250,40 +251,13 @@ const ServiceItem = ({ service, personal }: ServiceItemProps) => {
                     </div>
                   )}
 
-                  {selectedTime && selectedDay && (
+                  {selectedDate && (
                     <div className="p-5">
-                      <Card>
-                        <CardContent className="space-y-3 p-3">
-                          <div className="flex items-center justify-between">
-                            <h2 className="font-bold">{service.name}</h2>
-                            <p className="text-sm font-bold">
-                              {Intl.NumberFormat('pt-BR', {
-                                style: 'currency',
-                                currency: 'BRL',
-                              }).format(Number(service.price))}
-                            </p>
-                          </div>
-
-                          <div className="flex items-center justify-between">
-                            <h2 className="text-sm text-gray-400">Data</h2>
-                            <p className="text-sm">
-                              {format(selectedDay, "d 'de' MMMM", {
-                                locale: ptBR,
-                              })}
-                            </p>
-                          </div>
-
-                          <div className="flex items-center justify-between">
-                            <h2 className="text-sm text-gray-400">Horário</h2>
-                            <p className="text-sm">{selectedTime}</p>
-                          </div>
-
-                          <div className="flex items-center justify-between">
-                            <h2 className="text-sm text-gray-400">Personal</h2>
-                            <p className="text-sm">{personal.name}</p>
-                          </div>
-                        </CardContent>
-                      </Card>
+                      <BookingSummary
+                        personal={personal}
+                        service={service}
+                        selectedDate={selectedDate}
+                      />
                     </div>
                   )}
 
